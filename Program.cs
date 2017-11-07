924c127d85b2d8863e10f28a642b0eb29fd02e18
TwoZzz <33173816+TwoZzz@users.noreply.github.com>
2017-11-07 20:40:21 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+namespace Ex_0_TsN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str=String.Empty;
+            StreamWriter sw = new StreamWriter("file1.txt");
+            string str1 = Convert.ToString(Console.ReadLine());
+            sw.WriteLine(str1);
+            sw.Close();
+            StreamReader sr = new StreamReader("file1.txt");
+            str = sr.ReadToEnd();
+            Regex reg = new Regex(@"[0-9]{2}\.[0-9]{2}\.[0-9]{4}");
+            Match match = reg.Match(str);
+            sr.Close();
+            Console.WriteLine("\n");
+            StreamWriter f1 = new StreamWriter("file2.txt");
+            while (match.Success)
+            {
+                Console.WriteLine(match.Value);
+                f1.WriteLine(match.Value);
+                match = match.NextMatch();
+            }
+            f1.Close();
+            Console.WriteLine("\n");
+            StreamReader f2 = new StreamReader("file2.txt");
+            Console.WriteLine(f2.ReadToEnd());
+            f2.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Laba_0_reg_tn
